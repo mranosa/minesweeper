35089c16bd68d50558b8723986a00c746e1cf409
mranosa <mark.ranosa@irondata.com>
2013-06-17 04:57:09 +0800

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('minesweeperApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
