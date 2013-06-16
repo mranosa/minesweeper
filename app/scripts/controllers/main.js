f77470c48316a40fe8c6b905de809d06494cfd60
mranosa <mark.ranosa@irondata.com>
2013-06-17 05:12:07 +0800

    Initial grid implementation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,15 @@
 
 angular.module('minesweeperApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  	$scope.xSize = 8;
+  	$scope.ysize = 8;
+    $scope.grid = {};
+
+    function initGrid(xSize, ysize){
+    	for (var x = 0; x < xSize; x++) {
+    		for (var y = 0; y < ySize; y++) {
+  				$scope.grid[x][y].val = x + '-' + y;
+    		}	
+    	};
+    }
   });
