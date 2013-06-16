e2710d684abe8e61427450d9bcb79b1afb963fae
mranosa <mark.ranosa@irondata.com>
2013-06-17 05:26:26 +0800

    Tried another way to make it work :(

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,14 +3,17 @@
 angular.module('minesweeperApp')
   .controller('MainCtrl', function ($scope) {
   	$scope.xSize = 8;
-  	$scope.ysize = 8;
+  	$scope.ySize = 8;
     $scope.grid = {};
 
-    function initGrid(xSize, ysize){
+    function initGrid(xSize, ySize){
     	for (var x = 0; x < xSize; x++) {
     		for (var y = 0; y < ySize; y++) {
+    			if(!$scope.grid[x][y]) $scope.grid[x][y] = {};
   				$scope.grid[x][y].val = x + '-' + y;
     		}	
     	};
     }
+
+    initGrid(8, 8);
   });
